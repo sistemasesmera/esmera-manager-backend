53eb1fa67047543f24f1335cf8eed7ce497b9874
Sistemas <sistemas@esmeraschool.com>
2025-02-03 12:51:00 +0100

    pruebas

--- src/modules/webhook/webhook.controller.ts
+++ src/modules/webhook/webhook.controller.ts
@@ -6,7 +6,19 @@ export class WebhookController {
   test(@Request() request, @Body() body) {
     console.log('Headers:', request.headers);
     console.log('Body:', body); // Aquí ves el contenido JSON enviado por Builderall
-    console.log('Query Params:', request.query); // Si hay parámetros en la URL
+    console.log('Query Params:', request.query); // Si hay parámetros en la URL´
+
+    const nameLead = request.body.fname.toLowerCase();
+    const phoneLead = request.body.phone;
+    const emailLead = request.body.email;
+    const campaignLead = request.body.list_name;
+
+    console.log({
+      nameLead,
+      phoneLead,
+      emailLead,
+      campaignLead,
+    });
 
     return { message: 'Webhook received', data: body };
   }
